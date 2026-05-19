e0e5051f8f51577fa82d166763745972a9f3b6b2
Mihir Patel <mrpatel@g.clemson.edu>
2026-05-19 17:38:46 -0400

    Updated QuoteCard constructor for null safety and add callbacks

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -4,8 +4,10 @@ import 'quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  final Function delete;
-  QuoteCard({ this.quote, this.delete });
+  final VoidCallback delete;
+  final VoidCallback onLike;
+
+  const QuoteCard({super.key, required this.quote, required this.delete, required this.onLike});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,7 @@ class QuoteCard extends StatelessWidget {
                   color: Colors.grey[600],
                 ),
               ),
-              SizedBox(height: 6.0),
+              const SizedBox(height: 6.0),
               Text(
                 quote.author,
                 style: TextStyle(
