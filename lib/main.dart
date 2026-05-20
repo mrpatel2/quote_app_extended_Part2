592cdb01b03c841cd169ae43df2b0a0988fbe5f8
Mihir Patel <mrpatel@g.clemson.edu>
2026-05-19 23:21:26 -0400

    Implement setState logic for likes and delete buttons, and add categories

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,32 @@ import 'package:flutter/material.dart';
 import 'quote.dart';
 import 'quote_card.dart';
 
-void main() => runApp(MaterialApp(
-  home: QuoteList()
-));
+void main() => runApp(const MaterialApp(home: QuoteList()));
 
 class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
   @override
   _QuoteListState createState() => _QuoteListState();
 }
 
 class _QuoteListState extends State<QuoteList> {
-
   List<Quote> quotes = [
-    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
-    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
+    Quote(
+      author: 'Oscar Wilde',
+      text: 'Be yourself; everyone else is already taken',
+      category: 'Inspiration',
+    ),
+    Quote(
+      author: 'Oscar Wilde',
+      text: 'I have nothing to declare except my genius',
+      category: 'Humor',
+    ),
+    Quote(
+      author: 'Oscar Wilde',
+      text: 'The truth is rarely pure and never simple',
+      category: 'Truth',
+    ),
   ];
 
   @override
@@ -24,18 +35,30 @@ class _QuoteListState extends State<QuoteList> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Awesome Quotes'),
+        title: const Text('Awesome Quotes'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+        // We map through the quotes and pass the data and functions down to the QuoteCard
+        children: quotes
+            .map(
+              (quote) => QuoteCard(
+                quote: quote,
+                delete: () {
+                  setState(() {
+                    quotes.remove(quote);
+                  });
+                },
+                onLike: () {
+                  setState(() {
+                    quote.likes++;
+                  });
+                },
+              ),
+            )
+            .toList(),
       ),
     );
   }
 }
-
-
-
-
-
